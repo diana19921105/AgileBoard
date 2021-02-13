0de9be1874f549c24b018e9fda675784bf0328d5
Diana Szanto <szanto.diana.1992@gmail.com>
2021-02-13 09:23:19 +0100

    Initial commit

---
+++ src/main/java/hu/dianaszanto/agileboard/AgileBoardApplication.java
@@ -0,0 +1,13 @@
+package hu.dianaszanto.agileboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgileBoardApplication.class, args);
+    }
+
+}
